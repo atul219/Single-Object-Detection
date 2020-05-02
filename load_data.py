d34e82b8b66b7a65d7f85385e40ce86533329398
Atul Yadav <atulyadav219@gmail.com>
2020-05-02 22:58:57 +0530

    second commit

--- load_data.py
+++ load_data.py
@@ -47,9 +47,54 @@ for i in train_json[ANNOTATIONS]:
         b = i[BBOX]
         b = helper.convert_hw2bb(b)
         TRAIN_ANNOTAIONS[i[IMAGE_ID]].append((b, i[CATEGORY_ID]))
-        
+ 
+"""
+to draw image with bounding box and category name
+
+"""
 
 def draw_idx(i):
 	im_a = TRAIN_ANNOTAIONS[i]
 	img = helper.open_image(IMAGE_PATH/TRAIN_IMAGE[i])
 	helper.draw_img(img, im_a, CATEGORY_DICT)
+
+"""
+to get the largest annotations
+
+"""
+
+def get_largest_ann(b):
+	b = sorted(b, key = lambda x: np.product(
+		x[0][-2:] - x[0][:2]), reverse = True)
+	return b[0]
+
+TRAIN_LARGEST_ANNOTATIONS = {a: get_largest_ann(b) for a,b in TRAIN_ANNOTAIONS.items()}
+
+
+""" 
+to create csv files for bounding box and image category 
+
+"""
+
+(PATH/'csv_folder').mkdir(exist_ok = True)
+CSV_BB = PATH/'csv_folder/largest_bb.csv'
+
+CSV_CAT = PATH/'csv_folder/cat.csv'
+
+
+df_cat = pd.DataFrame({'file_name' : [TRAIN_IMAGE[o] for o in TRAIN_ID],
+					'category': [CATEGORY_DICT[TRAIN_LARGEST_ANNOTATIONS[o][1]] for o in TRAIN_ID]},
+					columns = ['file_name', 'category'])
+
+
+df_cat.to_csv(CSV_CAT, index = False)
+
+bb = np.array([TRAIN_LARGEST_ANNOTATIONS[o][0] for o in TRAIN_ID])
+
+bb_str = [' '.join(str(p) for p in o) for o in bb]
+
+df_bb = pd.DataFrame({'file_name': [TRAIN_IMAGE[o] for o in TRAIN_ID],
+					'bbs': bb_str},
+					columns = ['file_name', 'bbs'])
+
+df_bb.to_csv(CSV_BB, index = False)
